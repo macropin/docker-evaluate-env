5e46e3ebf7af28f2ce4f81f0424c1ca6e26ce8a2
Andrew Cutler <macropin@gmail.com>
2015-06-30 22:34:39 +1000

    bug fix

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ echo ">> Expanding Environment Variables from ${INPUT}"
 
 for ENV_FILE in `ls ${INPUT}`; do
     echo "Sourcing $ENV_FILE"
-    source $ENV_FILE
-    for e in `cat ${ENV_FILE}`; do
+    source ${INPUT}/$ENV_FILE
+    for e in `cat ${INPUT}/${ENV_FILE}`; do
         echo `eval "echo $e"` >> /tmp/env.out
     done
 done
